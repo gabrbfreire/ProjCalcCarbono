4d3a4c140e06d5d61aa7030c7f4f5c06f414e5af
Gabriel Freire <gabrbfreire@gmail.com>
2020-03-03 22:11:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projcalccarbono/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.projcalccarbono;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
